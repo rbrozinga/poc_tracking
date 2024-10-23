f00cc765108636fb2fcf320688f70ac84e379264
Richard Brozinga <richardbrozz@outlook.com>
2024-10-23 11:27:41 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Alert } from "react-native";
+import Mapbox from "@rnmapbox/maps";
+import * as Location from "expo-location";
+import polyline from "@mapbox/polyline";
+
+Mapbox.setAccessToken(
+  "pk.eyJ1IjoiZGFuaWxvbWlndWVsMTQ4NSIsImEiOiJjbGZwYzg2ZzQwdW0yM3FwdG91Z3BoZXVtIn0.FOkbq1V7d5cjKTXgyTQVuQ"
+);
 
 export default function App() {
+  const [routeCoords, setRouteCoords] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState([0, 0]);
+
+  useEffect(() => {
+    const startTracking = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permissão de localização negada!");
+        return;
+      }
+
+      // Obter localização inicial
+      let location = await Location.getCurrentPositionAsync({});
+      setCurrentLocation([location.coords.longitude, location.coords.latitude]);
+
+      // Começar a rastrear a posição do usuário
+      Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 1000,
+          distanceInterval: 5,
+        },
+        (newLocation) => {
+          const { latitude, longitude } = newLocation.coords;
+
+          setCurrentLocation([longitude, latitude]);
+
+          setRouteCoords((prevCoords) => [
+            ...prevCoords,
+            [longitude, latitude],
+          ]);
+        }
+      );
+    };
+
+    startTracking();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Mapbox.MapView style={styles.map} styleURL={Mapbox.StyleURL.Satellite}>
+        <Mapbox.Camera
+          zoomLevel={15}
+          centerCoordinate={currentLocation}
+          followUserMode="normal" // Centralizar a câmera no usuário
+        />
+
+        {/* Desenha a linha da rota */}
+        {routeCoords.length > 1 && (
+          <Mapbox.ShapeSource
+            id="routeSource"
+            shape={{
+              type: "LineString",
+              coordinates: routeCoords,
+            }}
+          >
+            <Mapbox.LineLayer
+              id="routeLine"
+              style={{
+                lineWidth: 4,
+                lineColor: "blue",
+              }}
+            />
+          </Mapbox.ShapeSource>
+        )}
+      </Mapbox.MapView>
     </View>
   );
 }
@@ -13,8 +82,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
   },
 });
